9855631d44b5b59c4a3964ac4034b3c349f94e94
faceless void <100186390+syd520zy@users.noreply.github.com>
2026-03-16 09:23:40 +0800

    Update utils.py

--- python/sglang/srt/entrypoints/openai/utils.py
+++ python/sglang/srt/entrypoints/openai/utils.py
@@ -80,9 +80,7 @@ def should_include_usage(
     # When stream_options are specified in the request
     if stream_options:
         include_usage = stream_options.include_usage or enable_force_include_usage
-        continuous_usage_stats = bool(
-            stream_options.continuous_usage_stats
-        )
+        continuous_usage_stats = bool(stream_options.continuous_usage_stats)
     else:
         include_usage, continuous_usage_stats = enable_force_include_usage, False
     return include_usage, continuous_usage_stats
